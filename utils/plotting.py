d6503d662cc8d16ad363cd1c28b28a6fc5d647f6
dabila <mboya4194@gmail.com>
2025-07-28 00:29:04 +0300

    Update plotting.py

--- utils/plotting.py
+++ utils/plotting.py
@@ -11,12 +11,25 @@ def plot_forecast_chart(forecast_df, model_name):
                       template="plotly_white", height=400)
     st.plotly_chart(fig, use_container_width=True)
 
-def plot_volatility_chart(forecast_df, volatility):
+import plotly.graph_objects as go
+
+def plot_volatility_chart(forecast_df, volatility_df):
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=forecast_df['Date'], y=forecast_df['Forecast'],
-                             mode='lines+markers', name='Forecast', line=dict(color='blue')))
-    fig.add_trace(go.Scatter(x=forecast_df['Date'], y=volatility,
-                             mode='lines+markers', name='Volatility', line=dict(color='red')))
-    fig.update_layout(title="GARCH Forecast & Volatility", xaxis_title="Date", yaxis_title="Value",
-                      template="plotly_white", height=400)
+
+    fig.add_trace(go.Scatter(
+        x=volatility_df.index,
+        y=volatility_df['Volatility'],
+        mode='lines+markers',
+        name='Volatility',
+        line=dict(color='orange', width=2)
+    ))
+
+    fig.update_layout(
+        height=400,
+        margin=dict(l=10, r=10, t=30, b=10),
+        template="plotly_white",
+        xaxis_title="Date",
+        yaxis_title="Volatility",
+    )
     st.plotly_chart(fig, use_container_width=True)
+
